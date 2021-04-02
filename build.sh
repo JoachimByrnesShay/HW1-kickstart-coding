877ec1c3c736f0f5fc635fc6b6bf948752008c1c
JoachimByrnesShay <me@Joachimbyrnesshay.com>
2021-04-02 03:00:46 -0400

    refined build.sh to use variables for repeated file locations

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 top=./templates/top.html
 bottom=./templates/bottom.html
+index=index.html
+about=about.html
+blog=blog.html
+projects=projects.html
 
-cat $top content/index.html $bottom > docs/index.html
+cat $top content/$index $bottom > docs/$index
 
