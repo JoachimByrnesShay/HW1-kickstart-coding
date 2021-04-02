652a5fe6bb10d9f82eab479dd475296aa2d358cb
JoachimByrnesShay <me@Joachimbyrnesshay.com>
2021-04-02 04:27:25 -0400

    partialled projects.html accordingly; added build projects.html in build.py; confimed docs/projects.html correctly functioning in browser

--- build.py
+++ build.py
@@ -10,7 +10,11 @@ PROJECTS = "projects.html"
 BUILD_INDEX = TOP + open(f"./content/{INDEX}").read() + BOTTOM
 BUILD_ABOUT = TOP + open(f"./content/{ABOUT}").read() + BOTTOM
 BUILD_BLOG = TOP + open(f"./content/{BLOG}").read() + BOTTOM
+BUILD_PROJECTS = TOP + open(f"./content/{PROJECTS}").read() + BOTTOM
 
 open(f"./docs/{INDEX}", "w+").write(BUILD_INDEX)
 open(f"./docs/{ABOUT}", "w+").write(BUILD_ABOUT)
 open(f"./docs/{BLOG}", "w+").write(BUILD_BLOG)
+open(f"./docs/{PROJECTS}", "w+").write(BUILD_PROJECTS)
+
+
