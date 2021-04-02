f1e49637baf6fb54436e4b7a443bccc3db06f9f6
JoachimByrnesShay <me@Joachimbyrnesshay.com>
2021-04-02 04:22:50 -0400

    partialed blog.html accordingly; added blog build line in build.py; confirmed in browser build.py correctly builds and links docs/blog.html

--- build.py
+++ build.py
@@ -9,6 +9,8 @@ PROJECTS = "projects.html"
 
 BUILD_INDEX = TOP + open(f"./content/{INDEX}").read() + BOTTOM
 BUILD_ABOUT = TOP + open(f"./content/{ABOUT}").read() + BOTTOM
+BUILD_BLOG = TOP + open(f"./content/{BLOG}").read() + BOTTOM
 
 open(f"./docs/{INDEX}", "w+").write(BUILD_INDEX)
 open(f"./docs/{ABOUT}", "w+").write(BUILD_ABOUT)
+open(f"./docs/{BLOG}", "w+").write(BUILD_BLOG)
