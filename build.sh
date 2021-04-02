e54c6a2be4c5ea552c990b47c3655478cbeafce6
JoachimByrnesShay <me@Joachimbyrnesshay.com>
2021-04-02 03:12:07 -0400

    partialled projects.html according to specs, added projects.html creation line to build.sh. confirmed all working functionality in browser for projects.html

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ projects=projects.html
 cat $top content/$index $bottom > docs/$index
 cat $top content/$about $bottom > docs/$about
 cat $top content/$blog $bottom > docs/$blog
-
+cat $top content/$projects $bottom > docs/$projects
