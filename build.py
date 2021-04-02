4a4769caa2b3cf6732fe0d12358abe11b6ccd187
JoachimByrnesShay <me@Joachimbyrnesshay.com>
2021-04-02 04:37:43 -0400

    tweaked build.py to use functions for building page content and writing pages to docs/

--- build.py
+++ build.py
@@ -7,14 +7,13 @@ ABOUT = "about.html"
 BLOG = "blog.html"
 PROJECTS = "projects.html"
 
-BUILD_INDEX = TOP + open(f"./content/{INDEX}").read() + BOTTOM
-BUILD_ABOUT = TOP + open(f"./content/{ABOUT}").read() + BOTTOM
-BUILD_BLOG = TOP + open(f"./content/{BLOG}").read() + BOTTOM
-BUILD_PROJECTS = TOP + open(f"./content/{PROJECTS}").read() + BOTTOM
+MY_PAGES = [INDEX, ABOUT, BLOG, PROJECTS]
 
-open(f"./docs/{INDEX}", "w+").write(BUILD_INDEX)
-open(f"./docs/{ABOUT}", "w+").write(BUILD_ABOUT)
-open(f"./docs/{BLOG}", "w+").write(BUILD_BLOG)
-open(f"./docs/{PROJECTS}", "w+").write(BUILD_PROJECTS)
+def build_pg_content(page):
+    return TOP + open(f"./content/{page}").read() + BOTTOM
 
+def write_pg(page, full_content):
+    open(f"./docs/{page}", "w+").write(full_content)
 
+for pg in MY_PAGES:
+    write_pg(pg, build_pg_content(pg))
