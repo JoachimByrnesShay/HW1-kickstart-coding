76fa27f14a2a31161eb7cb7d53f5a7b695440634
JoachimByrnesShay <me@Joachimbyrnesshay.com>
2021-04-02 09:50:27 -0400

    add functions for building full page content and for writing new pages with that content; apply these functions by iterating through the list of page filenames and passing the filename of each page to the functions

--- build.py
+++ build.py
@@ -7,3 +7,15 @@ BLOG = "blog.html"
 PROJECTS = "projects.html"
 
 MY_PAGES = [INDEX, ABOUT, BLOG, PROJECTS]
+
+
+def build_pg_content(page):
+    return TOP + open(f"./content/{page}").read() + BOTTOM
+
+
+def write_pg(page, full_content):
+    open(f"./docs/{page}", "w+").write(full_content)
+
+
+for pg in MY_PAGES:
+    write_pg(pg, build_pg_content(pg))
