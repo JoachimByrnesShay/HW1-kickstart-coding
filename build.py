cbdb10069dc3b4103333b5ddd6284b90061df079
JoachimByrnesShay <me@Joachimbyrnesshay.com>
2021-04-08 23:33:10 -0400

    phase 3 complete; combined 2 templates into single base.html template; adjusted build.py script accordingly, using String#format in script and {} in template instead of String#replace and {{}} inside template

--- build.py
+++ build.py
@@ -1,14 +1,13 @@
 import modules.pages
 
 def main():
-    top = open("templates/top.html").read()
-    bottom = open("templates/bottom.html").read()
+    base = open('templates/base.html').read()
 
     pages = modules.pages.PAGES
 
     for this_page in pages:
         main_content = open(this_page['filename']).read()
-        new_content = top + main_content + bottom
+        new_content = base.format(content=main_content) 
         open(this_page['output'], 'w+').write(new_content)
 
 main()
