cfe8d0ff52154e6cc9afcb822c88362822c18679
JoachimByrnesShay <me@Joachimbyrnesshay.com>
2021-04-09 00:11:58 -0400

    added {links} placeholder in base.html; added create_links() function to generate li with html links, which adds 'active' to the inline class list for links as appropriate for the current page; create_links() is called per each page inside of create_output() function to generate the full list of li > a per each page

--- build.py
+++ build.py
@@ -1,15 +1,28 @@
 import modules.pages
 
-def apply_template(content, title):
+def apply_template(title, links, content):
     template = open("templates/base.html").read()
-    return template.format(title=title, content=content)
+    return template.format(title=title, links=links, content=content)
+
+def create_links(pages, page):
+    list_links = ''
+    li_with_link = '<li class="nav-item"><a class="nav-link text-white {active_class}" href="{link_href}">{link_title}</a></li>'
+    for this_page in pages:
+        if this_page['title'] == page['title']:
+            active_class = "active"
+        else:
+            active_class = ""
+        link_href = this_page['output'].split('/')[1]
+        link_title = this_page['title'].upper()
+        list_links += li_with_link.format(active_class=active_class, link_href=link_href, link_title=link_title)
+    return list_links
 
 def create_output(pages):
     for this_page in pages:
         main_content = open(this_page['filename']).read()
+        links = create_links(pages, this_page)
         title = this_page['title'].upper()
-        open(this_page['output'], 'w+').write(apply_template(title=title, content=main_content))
-
+        open(this_page['output'], 'w+').write(apply_template(title=title, links=links, content=main_content))
 
 
 def main():
