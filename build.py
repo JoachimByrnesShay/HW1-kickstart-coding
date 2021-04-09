39b3c88adc6af9251b1ff35936d602bf284f8283
JoachimByrnesShay <me@Joachimbyrnesshay.com>
2021-04-08 23:52:40 -0400

    added {title} placeholder in base template; added title param to apply_template function;  title on output pages is working

--- build.py
+++ build.py
@@ -1,18 +1,19 @@
 import modules.pages
 
-def apply_template(content):
+def apply_template(content, title):
     template = open("templates/base.html").read()
-    return template.format(content=content)
+    return template.format(title=title, content=content)
 
 def create_output(pages):
     for this_page in pages:
         main_content = open(this_page['filename']).read()
-        open(this_page['output'], 'w+').write(apply_template(main_content))
+        title = this_page['title'].upper()
+        open(this_page['output'], 'w+').write(apply_template(title=title, content=main_content))
 
 
 
 def main():
     pages = modules.pages.PAGES
     create_output(pages)
-    
+
 main()
