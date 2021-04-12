f24fc9671fb17f4919f876ef5d5366f4407c699d
JoachimByrnesShay <me@Joachimbyrnesshay.com>
2021-04-11 20:21:31 -0400

    finished fix of relative links based upon adjustment to hosting blogs/ from inside docs/ folder;  finished comments in build.py accordingly

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ def get_year():
 
 def apply_base_template(title, links, content, css_pdir='./'):
     """inserts page content, title, page nav links, current year into template and returns full content
-    adjusts css href as css_path if needed"""
+    adjusts css href  with css_pdir prepended if needed for relative links based on parent directory of css folder, as needed from inside docs/blogs"""
     year = get_year()
     template = open("templates/base.html").read()
     template_vars = {'title':title, 'links':links, 'content':content, 'year': year, 'css_pdir': css_pdir}
@@ -31,7 +31,7 @@ def get_link_classes(this_page, compared_page):
 def create_li_links(pages, curr_page, links_pdir='./'):
     """creates set of html li > a links to be inserted into placeholder via templatig inside of ul.navbar-nav 
     with appropriate href, link text, and adds .active class to inline class list for link where required.
-    main_pages_location variable allows to adjust path relative to folder (such as for links in blog/1.html)"""
+    links_pdir variable allows to adjust path relative to parent directory (such as for links in blog/1.html)"""
     list_links = ''
     li_with_link = '<li class="nav-item"><a class="{link_classes}" href="{link_href}">{link_title}</a></li>'
     for page in pages:
@@ -41,15 +41,15 @@ def create_li_links(pages, curr_page, links_pdir='./'):
         list_links += li_with_link.format(link_classes=link_classes, link_href=link_href, link_title=link_title)
     return list_links
 
-def create_blog_pages(pages, blogs, blog_dir='docs/'):
-    """creates individual blog pages in blog/"""
-    # pending further development, currently for blog pages no link in nav is set to active
+def create_blog_pages(pages, blogs, blog_pdir='docs/'):
+    """creates individual blog pages in blog/.  blog_dir adjusts relative links based on parent dir of blogs dir.  current site is hosted from docs/ on github pages.
+    as implemented, for blog pages there are no links in nav set to active state, pasing None as curr_page in create_li_links() below insures that"""
     links = create_li_links(pages, curr_page=None, links_pdir='../')
+    # pass blog_base.html as content to base.html in apply_base_template() function
     blog_base = open('templates/blog_base.html').read()
-    # pass blog_base.html as content to base.html in apply_base_template() fuunction
     blog_content_template = apply_base_template(title="Blog Item", links=links, content=blog_base, css_pdir='../')
     for blog in blogs:
-        file_name = open(blog_dir + blog['filename'], 'w+')
+        file_name = open(blog_pdir + blog['filename'], 'w+')
         blog_vars = {'blog_item_title': blog['title'], 'blog_item_date': blog['date'], 'blog_item_content': blog['content']}
         file_name.write(blog_content_template.format(**blog_vars))
 
@@ -64,7 +64,8 @@ def create_main_pages(pages, blogs):
         open(this_page['output'], 'w+').write(apply_base_template(title=title, links=links, content=main_content))
 
 def create_blog_index(blogs):
-    """ creates content for docs/blog.html consisting of indexing of blog pages by title and 30 character lead"""
+    """ creates content for docs/blog.html consisting of indexing of blog pages by title and 30 character lead.
+    uses BLOG_POSTS in modules/blog_posts.py"""
     item_template = open('templates/blog_index_item.html').read()
     content = ''
     for blog_item in blogs:
